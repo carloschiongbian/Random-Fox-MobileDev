6485ffb9a85cd0efc531b221fc8af08b3ef744a2
RichLim <richlim16@gmail.com>
2021-06-16 22:54:23 +0800

    cleaned up validation

--- mobiledev_final_project/lib/mixin/ValidationMixin.dart
+++ mobiledev_final_project/lib/mixin/ValidationMixin.dart
@@ -6,21 +6,9 @@ class ValidationMixin {
     return (value.contains("@")) ? null : "Invalid email format";
   }
 
-  String validateAddress(String value){
-    return (value.length >= 10) ? null : "Address field cannot be lesser than 10 characters.";
-  }
-
-  String validateName(String value){
-    return (value.length >= 3 && value.length <= 10) ? null : "Name field cannot be less than 3 characters and more than 10 characters.";
-  }
-
   String validatePassword(String value){
     this.password = value;
     return (value.length >= 8) ? null : "Invalid password format. Must be at least 8 characters.";
-  }
-
-  String validateEstablishment(String value){
-    return (value.length < 5) ? null : "Establishment name cannot be less than 5 characters.";
   }
 
   String confirmPassword(String value){
